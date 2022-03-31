1fd2aad9d6b422878d0bb8be1f343b8fd897b1db
mirkosimunovic <mirko.simunovic@gmail.com>
2022-03-31 11:09:01 -1000

    add email address

--- layouts.py
+++ layouts.py
@@ -5,6 +5,7 @@ import dash_bootstrap_components as dbc
 about_layout = html.Div([dbc.Col([
                         html.Br(),html.Br(),html.Br(),html.Br(),
                         html.H1('Información',style={'textAlign':'left'}),
+                        html.Br(),
                         html.Div(html.P(['Esta aplicación usa datos de ',html.A(href='https://developers.google.com/maps/documentation/directions/overview',
                                                                                children='Google Directions API'),
                                          ' para comparar las rutas de viaje entre Bicicleta y Transporte Público en Santiago.'])),
@@ -21,8 +22,12 @@ about_layout = html.Div([dbc.Col([
 contact_layout = html.Div([dbc.Col([
                         html.Br(),html.Br(),html.Br(),html.Br(),    
                         html.H1('Contacto',style={'textAlign':'center'}),
+                        html.Br(),
                         html.P(['Puedes revisar el codigo en mi ',html.A(href='http://www.github.com/mirkosimunovic/biciometro',children='Github.')],style={'textAlign':'center'}),
                         html.Br(),
-                        html.P(['Me puedes encontrar en ',html.A(href='http://www.linkedin.com/in/mirko-simunovic/',children='LinkedIn.')],style={'textAlign':'center'})
+                        html.P(['Me puedes encontrar en ',html.A(href='http://www.linkedin.com/in/mirko-simunovic/',children='LinkedIn.')],style={'textAlign':'center'}),
+                        html.Br(),
+                        html.P(['Email: ',html.A(href='mailto:mirko.simunovic@gmail.com',children='mirko.simunovic@gmail.com')],style={'textAlign':'center'})
+
                         ],width={'offset':0})
                         ])
